089cef602cc7f1e4125c1f177afb1a7d975b9cbf
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-04-14 05:11:10 +0200

    finished tutorial, more features in the future

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -4,32 +4,13 @@ import { projectFirestore } from '../firebase/config';
 const useFirestore = (collection) => {
     const [docs, setDocs] = useState([]);
 
-    // useEffect(() => {
-    //     const unsub = projectFirestore.collection(collection)
-    //         .orderBy('createdAd', 'desc')
-    //         .onSnapshot((snap) => {
-    //             console.log(snap[0]);
-    //             let documents = [];
-    //             snap.forEach(doc => {
-    //                 documents.push({...doc.data(), id: doc.id});
-    //                 console.log(snap);
-    //             });
-    //             setDocs(documents);
-    //         });
-
-    //     return () => unsub();
-
-    // }, [collection])
-
     useEffect(() => {
         const unsub = projectFirestore.collection(collection)
-            // .orderBy('createdAd', 'desc')
+            .orderBy('createdAt', 'desc')
             .onSnapshot((snap) => {
-                console.log(snap[0]);
                 let documents = [];
                 snap.forEach(doc => {
                     documents.push({...doc.data(), id: doc.id});
-                    console.log(snap);
                 });
                 setDocs(documents);
             });
